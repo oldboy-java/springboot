1cae05b7612484fcfe78062226a825b49704be84
liuli <liuli123456@163.com>
2018-08-21 21:12:40 +0800

    增加实体类

---
+++ chaper3/src/main/java/com/liuli/pojo/Author.java
@@ -0,0 +1,14 @@
+package com.liuli.pojo;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2018-8-21.
+ */
+
+@Data
+public class Author {
+    private String name;
+    private Integer age;
+    private String email;
+}
