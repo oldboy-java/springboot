a1563b460cf7fd9fabd4332a24e9a82efd4defda
oldboy-java <liuli20061014@163.com>
2019-01-20 21:35:01 +0800

    过滤器中返回json数据处理、增加json工具类

--- girl/src/main/java/com/imooc/girl/filter/AuthFilter.java
+++ girl/src/main/java/com/imooc/girl/filter/AuthFilter.java
@@ -1,7 +1,7 @@
 package com.imooc.girl.filter;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -12,13 +12,13 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.alibaba.fastjson.JSONObject;
-import com.imooc.girl.dao.GirlRespository;
 import com.imooc.girl.service.GirlService;
+import com.imooc.girl.utils.JsonUtils;
 import com.imooc.girl.utils.SpringContextUtils;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class AuthFilter implements Filter {
 	
 	@Override
@@ -46,25 +46,47 @@ public class AuthFilter implements Filter {
 			
 			System.err.println("url="+url+"?"+query+"&t="+ (girlService == girlService2 ? true :false));
 			try {
-			//	response(resp);
-				resp.sendRedirect("login.html");
+				JSONObject jsonObject = new JSONObject();
+				jsonObject.put("code",401);
+				jsonObject.put("message", "静态页面不能直接访问");
+				
+				response(resp,jsonObject);
+			//	resp.sendRedirect("login.html");
 				
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		chain.doFilter(request, response);
 	}
 
-	private void response(HttpServletResponse resp)  throws Exception{
-		
-		resp.setContentType("application/json; charset=utf-8");
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("code",401);
-		jsonObject.put("message", "静态页面不能直接访问");
-		resp.getOutputStream().write(jsonObject.toJSONString().getBytes("UTF-8"));
+	private void response(HttpServletResponse resp,JSONObject jsonObject)  throws Exception{
+			resp.setContentType("application/json; charset=utf-8");
+			OutputStream out=null;
+			try{
+			    out = resp.getOutputStream();
+			    out.write(jsonObject.toString().getBytes("utf-8"));
+			    out.flush(); //必须调用flush或者close方法，否则ContentType不起作用
+			} finally{
+			    if(out!=null){
+			        out.close();
+			    }
+			}
 	}
+	
+	/*private void  response(HttpServletResponse resp,Object obj)  throws Exception{
+		OutputStream out=null;
+		resp.setContentType("application/json; charset=utf-8");
+		try{
+		    out = resp.getOutputStream();
+		    out.write(JsonUtils.objectToJson(obj).getBytes("utf-8"));
+		    out.flush();
+		} finally{
+		    if(out!=null){
+		        out.close();
+		    }
+		}
+	}*/
 	
 	@Override
 	public void destroy() {
